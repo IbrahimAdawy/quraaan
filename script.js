3b75847c3891aae45c5953cab397fd8df0b403c5
IbrahimAdawy <113234855+IbrahimAdawy@users.noreply.github.com>
2024-12-07 16:10:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const radioPlayer = document.getElementById("radioPlayer");
+  
+    // You can add additional features, such as play/pause buttons or volume control, here
+    console.log("Radio Player Loaded!");
+  });
+  
